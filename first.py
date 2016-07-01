53f460124a3d487ad34f69fd7fba99028db5e7b6
411745019@qq.com <Saber@XuMingQideMac-mini.local>
2016-07-01 15:18:32 +0800

    jav

--- first.py
+++ first.py
@@ -6,13 +6,17 @@ import threading, queue
 
 
 def saveImg(url, fileName):
-    print('saving...' + fileName)
-    print('count', threading.active_count())
     u = request.urlopen(url)
     data = u.read()
     f = open(fileName, 'wb')
     f.write(data)
     f.close()
+
+def addWork():
+    for i in range(0, 100):
+        print('add work size :',works.qsize())
+        works.put((saveImg, list((images[1], path + '/' + names[1] + '.jpg'))))
+        time.sleep(1)
 
 
 class Work(threading.Thread):
@@ -21,10 +25,13 @@ class Work(threading.Thread):
         threading.Thread.__init__(self)
         self.start()
 
-
     def run(self):
         while True:
             try:
+                if self.work_queue.empty():
+                    time.sleep(1)
+                    print('sleep over')
+                    continue;
                 dofun, args = self.work_queue.get(block=False)
                 dofun(args[0], args[1])
                 self.work_queue.task_done()
@@ -32,31 +39,44 @@ class Work(threading.Thread):
                 print('ex')
                 break;
 
-
+path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path, os.pardir, 'pic')
 userAgent = "Mozilla/5.0"
 headers = {'User-Agent': userAgent}
-url = 'http://www.xiami.com'
+url = 'https://www.javbus.com/page/'
+page = 10
+htmlThreads = []
+for i in range(1,page+1):
+    tmpUrl = url+str(i)
+    req = request.Request(tmpUrl, headers=headers)
+    res = request.urlopen(req)
+    content = res.read().decode('utf-8')
+    f = open(path+'/'+str(i)+'_page.html', 'w')
+    f.write(content)
+    f.close()
+exit()
 req = request.Request(url, headers=headers)
 res = request.urlopen(req)
 
 content = res.read().decode('utf-8')
+
+
+
+
 patternName = re.compile('<p class="name"><strong><a.*?>(.*?)</a')
 patternImg = re.compile('<div class="album">\s*<div class="image">\s*<img src="(.*?)"')
 names = re.findall(patternName, content)
 images = re.findall(patternImg, content)
-path = os.path.dirname(os.path.abspath(__file__))
-path = os.path.join(path, os.pardir, 'pic')
 
 tmpTime = time.time()
 threads = []
 works = queue.Queue()
 
-tmpCnt = 1
-for i in range(0, tmpCnt * len(images)):
-    idx = int(i / tmpCnt)
-    works.put((saveImg, list((images[idx], path + '/' + names[idx] + '.jpg'))))
+addWorkThread = threading.Thread(target=addWork)
+threads.append(addWorkThread)
+addWorkThread.start()
 
-maxTCnt = works.qsize()
+maxTCnt = 5
 for i in range(0, maxTCnt):
     w = Work(works)
     threads.append(w)
